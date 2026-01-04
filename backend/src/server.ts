3be55fedfb101fdf022364303d1ebb147ab5dc2a
Jonas Thommen <jonas.thommen@gmail.com>
2026-01-04 20:59:42 +0100

    change server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from "path";
 import cors from 'cors';
 import { initDatabase } from './config/database';
 import authRoutes from './routes/auth';
@@ -10,6 +11,7 @@ import { startReminderJob } from './jobs/reminderJob';
 const app = express();
 const PORT = 3000;
 
+
 app.use(cors());
 app.use(express.json());
 
@@ -17,6 +19,26 @@ app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/devices', deviceRoutes);
 app.use('/api/debug', debugRoutes); // Debug-Routes
+
+const angularDistPath = path.resolve(
+  __dirname,
+  "../../frontend/dist/frontend"
+);
+
+// Static Files
+app.use(express.static(angularDistPath, {
+  maxAge: "1y",
+  index: false, // wir liefern index.html unten gezielt aus
+}));
+
+// 3) SPA fallback (nur für nicht-API requests)
+app.get("*", (req, res, next) => {
+  // API nicht schlucken
+  if (req.path.startsWith("/api")) return next();
+
+  res.sendFile(path.join(angularDistPath, "index.html"));
+});
+
 
 async function startServer() {
   await initDatabase();
