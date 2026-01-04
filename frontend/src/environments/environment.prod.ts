43a25edda6b22b41ba694d6870ff9ff839a97619
Jonas Thommen <jonas.thommen@gmail.com>
2026-01-04 21:29:08 +0100

    fix: update API URL in production environment configuration

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://retter.swissgliders.ch:3000/api'  // Oder separate Backend-URL
+  apiUrl: 'https://retter.swissgliders.ch/api'  // Oder separate Backend-URL
 };
